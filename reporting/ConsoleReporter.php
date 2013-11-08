57258eaada04f9bc3fae2958e80fdafd65d07801
Marko Simovic <msimovic@cuantile.com>
2013-11-08 05:06:02 -0500

    Updated consolereporter to print depth in excel-friendly format

--- reporting/ConsoleReporter.php
+++ reporting/ConsoleReporter.php
@@ -14,7 +14,27 @@ class ConsoleReporter implements IReporter
 
     public function depth($exchange_name, $currencyPair, $depth){
         print "$exchange_name - $currencyPair\n";
-        var_dump($depth);
+
+        print "Bid Quantity, Bid Price, Ask Price, Ask Quantity\n";
+        for($i = 0; $i < max(count($depth['bids']),count($depth['asks']));$i++){
+            if($i < count($depth['bids'])){
+                $px = $depth['bids'][$i][0];
+                $qty = $depth['bids'][$i][1];
+                print "$qty,$px";
+            }else
+                print ",";
+
+            print ",";
+
+            if($i < count($depth['asks'])){
+                $px = $depth['asks'][$i][0];
+                $qty = $depth['asks'][$i][1];
+                print "$px,$qty";
+            }else
+                print ",";
+
+            print "\n";
+        }
     }
     
     public function trades($exchange_name, $currencyPair, $trades){
