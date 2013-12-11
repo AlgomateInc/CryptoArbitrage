826f22c8cd5a934231a0ebb360e7fec3cb16c761
Marko Simovic <msimovic@cuantile.com>
2013-12-11 14:25:56 -0500

    Externalized btce auth parts to subclass. Confirmed working

---
+++ markets/BtceStyleExchange.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once(__DIR__.'/../config.php');
+require_once(__DIR__.'/../curl_helper.php');
+require_once('IExchange.php');
+require_once(__DIR__.'/../OrderExecution.php');
+require_once('NonceFactory.php');
+
+
+abstract class BtceStyleExchange implements IExchange {
+
+    abstract protected function getAuthQueryUrl();
+
+    private $key;
+    private $secret;
+    private $nonceFactory;
+
+    public function __construct($key, $secret){
+        $this->key = $key;
+        $this->secret = $secret;
+
+        $this->nonceFactory = new NonceFactory();
+    }
+
+    protected function assertSuccessResponse($response)
+    {
+        if($response['success'] != 1)
+            throw new Exception($response['error']);
+
+        return $response;
+    }
+
+    protected function authQuery($method, array $req = array()) {
+        if(!$this->nonceFactory instanceof NonceFactory)
+            throw new Exception('No way to get nonce!');
+
+        $req['method'] = $method;
+        $req['nonce'] = $this->nonceFactory->get();
+
+        // generate the POST data string
+        $post_data = http_build_query($req, '', '&');
+
+        $sign = hash_hmac("sha512", $post_data, $this->secret);
+
+        // generate the extra headers
+        $headers = array(
+            'Sign: '.$sign,
+            'Key: '.$this->key,
+        );
+
+        return curl_query($this->getAuthQueryUrl(), $post_data, $headers);
+    }
+
+} 
--- markets/btce.php
+++ markets/btce.php
@@ -1,22 +1,13 @@
 <?php
 
-require_once(__DIR__.'/../config.php');
 require_once(__DIR__.'/../curl_helper.php');
-require_once('IExchange.php');
 require_once(__DIR__.'/../OrderExecution.php');
-require_once('NonceFactory.php');
+require_once('BtceStyleExchange.php');
 
-class BtceExchange implements IExchange
+class BtceExchange extends BtceStyleExchange
 {
-    private $key;
-    private $secret;
-    private $nonceFactory;
-
-    public function __construct($key, $secret){
-        $this->key = $key;
-        $this->secret = $secret;
-
-        $this->nonceFactory = new NonceFactory();
+    protected function getAuthQueryUrl(){
+        return 'https://btc-e.com/tapi/';
     }
 
     public function Name(){
@@ -188,33 +179,5 @@ class BtceExchange implements IExchange
         return $execList;
     }
 
-    function assertSuccessResponse($response)
-    {
-        if($response['success'] != 1)
-            throw new Exception($response['error']);
-
-        return $response;
-    }
-
-    function authQuery($method, array $req = array()) {
-        if(!$this->nonceFactory instanceof NonceFactory)
-            throw new Exception('No way to get nonce!');
-
-        $req['method'] = $method;
-        $req['nonce'] = $this->nonceFactory->get();
-
-        // generate the POST data string
-        $post_data = http_build_query($req, '', '&');
-
-        $sign = hash_hmac("sha512", $post_data, $this->secret);
-
-        // generate the extra headers
-        $headers = array(
-            'Sign: '.$sign,
-            'Key: '.$this->key,
-        );
-
-        return curl_query('https://btc-e.com/tapi/', $post_data, $headers);
-    }
 }
 
