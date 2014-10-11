0e83d161d59c7b2fa739e4bdcd7cdcbb5ef71190
Marko Simovic <markobarko@gmail.com>
2014-10-11 08:16:42 -0400

    Fixed issue on size factor loading from config file

--- arbinstructions/ConfigArbInstructionLoader.php
+++ arbinstructions/ConfigArbInstructionLoader.php
@@ -12,23 +12,39 @@ class ConfigArbInstructionLoader implements IArbInstructionLoader {
 
         foreach($configArbInstructions as $cfgArb)
         {
-            $arb = new ArbInstructions();
-            $arb->currencyPair = $cfgArb['currencyPair'];
-            $arb->buyExchange = $cfgArb['from'];
-            $arb->sellExchange = $cfgArb['to'];
-
-            foreach($cfgArb['factors'] as $f)
-            {
-                $factor = new ArbExecutionFactor();
-                $factor->targetSpreadPct = $f['spreadPct'];
-                $factor->maxUsdOrderSize = $f['maxUsdSize'];
-                $factor->orderSizeScaling = $f['sizeScaling'];
-
-                $arb->arbExecutionFactorList[] = $factor;
-            }
-
-            $this->arbInst[] = $arb;
+            $sail = new SingleArbInstructionLoader($cfgArb);
+            $this->arbInst[] = $sail->load();
         }
+    }
+
+    public function load()
+    {
+        return $this->arbInst;
+    }
+}
+
+class SingleArbInstructionLoader implements IArbInstructionLoader {
+
+    private $arbInst;
+
+    public function __construct($cfgArb)
+    {
+        $arb = new ArbInstructions();
+        $arb->currencyPair = $cfgArb['currencyPair'];
+        $arb->buyExchange = $cfgArb['from'];
+        $arb->sellExchange = $cfgArb['to'];
+
+        foreach($cfgArb['factors'] as $f)
+        {
+            $factor = new ArbExecutionFactor();
+            $factor->targetSpreadPct = $f['spreadPct'];
+            $factor->maxUsdOrderSize = $f['maxUsdSize'];
+            $factor->orderSizeScaling = $f['sizeFactor'];
+
+            $arb->arbExecutionFactorList[] = $factor;
+        }
+
+        $this->arbInst = $arb;
     }
 
     public function load()
