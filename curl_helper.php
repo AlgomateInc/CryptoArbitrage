321046aa3c4835b8367266c1e95511cd574e76d8
Marko Simovic <markobarko@gmail.com>
2015-02-17 15:35:54 -0500

    Set explicit timeouts on all curl queries. Seems to fix the issue of hanging curl_exec

--- curl_helper.php
+++ curl_helper.php
@@ -5,6 +5,8 @@ function curl_query($url, $post_data = null, $headers = array()){
     if (is_null($ch)) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
     }
     curl_setopt($ch, CURLOPT_URL, $url);
