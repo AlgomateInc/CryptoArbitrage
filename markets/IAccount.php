7f6ae3bf78bd2246712f3ab3369d1059a6fd9ea6
Marko Simovic <markobarko@gmail.com>
2016-01-17 19:35:26 -0500

    Updated phpdoc for IAccount and IExchange to specify more in/out types

--- markets/IAccount.php
+++ markets/IAccount.php
@@ -2,7 +2,14 @@
 
 interface IAccount {
 
+    /**
+     * @return String Simple name for account or account provider
+     */
     public function Name();
+
+    /**
+     * @return array An associative array mapping Currency to numerical balance
+     */
     public function balances();
     public function transactions();
 
--- markets/IExchange.php
+++ markets/IExchange.php
@@ -16,19 +16,35 @@ interface IExchange extends IAccount
     public function supportedCurrencyPairs();
 
     /**
-     * @param $pair The pair we want to get minimum order size for
-     * @param $pairRate Supply a price for the pair, in case the rate is based on quote currency
+     * @param $pair String The CurrencyPair pair we want to get minimum order size for
+     * @param $pairRate float Supply a price for the pair, in case the rate is based on quote currency
      * @return mixed The minimum order size, in the base currency of the pair
      */
     public function minimumOrderSize($pair, $pairRate);
 
     /**
-     * @return array Provides an array of strings listing supported currencies
+     * @return string[] Provides an array of strings listing supported currencies
      */
     public function supportedCurrencies();
 
+    /**
+     * @param $pair String The CurrencyPair string that we want ticker information for
+     * @return Ticker An object with last tick information about the pair
+     */
     public function ticker($pair);
+
+    /**
+     * @param $pair String The CurrencyPair string that we want last trade data for
+     * @param $sinceDate int The unix timestamp of the date we are interested in
+     * @return Trade[] An array of Trade objects, representing each trade that
+     * has occurred since sinceDate
+     */
     public function trades($pair, $sinceDate);
+
+    /**
+     * @param $currencyPair String The CurrencyPair string that we want depth data for
+     * @return OrderBook The object containing order book depth data for the pair
+     */
     public function depth($currencyPair);
 
     public function buy($pair, $quantity, $price);
