42e31dd36e116e654a72ca09de7ddf0cf34a9948
Marko Simovic <markobarko@gmail.com>
2016-12-01 06:23:35 -0500

    Balances are now reported every 8 hours regardless of change

--- trading/BalanceManager.php
+++ trading/BalanceManager.php
@@ -11,10 +11,13 @@ class BalanceManager {
     private $reporter;
 
     private $balances = array();
+    private $lastReportTime;
+    const ForceReportAfterSeconds = 28800; //8 hours
 
     function __construct(IReporter $reporter)
     {
         $this->reporter = $reporter;
+        $this->lastReportTime = time();
     }
 
     function get($marketName, $currencyPair)
@@ -49,9 +52,15 @@ class BalanceManager {
         }
 
         //update our running list of balances
+        $forceReport = false;
+        if(time() > $this->lastReportTime + self::ForceReportAfterSeconds){
+            $forceReport = true;
+            $this->lastReportTime = time();
+        }
         foreach($balList as $cur => $bal){
-            //report balance only on balance change (or first run)
-            if(!isset($this->balances[$mkt->Name()][$cur]) || $this->balances[$mkt->Name()][$cur] != $bal)
+            //report balance only on balance change (or first run, or periodically)
+            if(!isset($this->balances[$mkt->Name()][$cur]) ||
+                $this->balances[$mkt->Name()][$cur] != $bal || $forceReport)
                 $this->reporter->balance($mkt->Name(), $cur, $bal);
 
             $this->balances[$mkt->Name()][$cur] = $bal;
