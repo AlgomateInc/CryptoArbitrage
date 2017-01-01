f53ebc2d7994b242ca4ee4102ad74fd01aef4223
Marko Simovic <markobarko@gmail.com>
2017-01-01 14:03:37 -0500

    Removed DAOETH pair as delisted

--- markets/Kraken.php
+++ markets/Kraken.php
@@ -74,7 +74,7 @@ class Kraken extends BaseExchange implements ILifecycleHandler
 
     public function supportedCurrencyPairs()
     {
-        return array(CurrencyPair::BTCUSD, CurrencyPair::ETHBTC, CurrencyPair::ETHUSD, CurrencyPair::DAOETH);
+        return array(CurrencyPair::BTCUSD, CurrencyPair::ETHBTC, CurrencyPair::ETHUSD);
     }
 
     public function minimumOrderSize($pair, $pairRate)
