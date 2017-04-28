d58b25322e373d54816e1a70d3f8119cf60de80e
Jon Cinque <jon.cinque@gmail.com>
2017-04-28 16:16:00 +0100

    Add Docker configurations for all environments

--- deploy/docker_start.sh
+++ deploy/docker_start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 /usr/bin/mongod & sleep 5
-/usr/bin/php /CryptoArbitrage/MarketDataMonitor.php --mongodb --monitor=10 --discard-depth --balances &
+/usr/bin/php /CryptoArbitrage/MarketDataMonitor.php --mongodb --monitor=10 --discard-depth &
 /usr/bin/php /CryptoArbitrage/StrategyProcessor.php --mongodb --monitor --live &
-/usr/bin/php /CryptoArbitrage/ReportingServer.php --mongodb --monitor=0 &
+/usr/bin/php /CryptoArbitrage/ReportingServer.php --mongodb --monitor=0
